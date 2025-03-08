263d39d7f03e33382469c19b0dd70a7fbd433fcc
Profile-exe <kang67572346@gmail.com>
2025-03-08 22:17:19 +0900

    chore: Spring Boot 프로젝트 생성
    - actuator
    - prometheus
    - jpa
    - quartz
    - security
    - validation
    - web
    - lombok
    - h2
    - mysql

---
+++ src/main/java/com/jnulocker/JnuLockerApplication.java
@@ -0,0 +1,13 @@
+package com.jnulocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JnuLockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JnuLockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jnulocker/JnuLockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnulocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnuLockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
