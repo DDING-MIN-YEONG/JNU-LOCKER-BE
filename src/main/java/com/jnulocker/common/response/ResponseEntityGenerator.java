fb64467edbdda96cf75b2e14f6aecc736475e308
westzeroright <kt8267@naver.com>
2025-03-24 23:02:23 +0900

    refactor: data로 한번 더 감싸지 않음

--- src/main/java/com/jnulocker/common/response/ResponseEntityGenerator.java
+++ src/main/java/com/jnulocker/common/response/ResponseEntityGenerator.java
@@ -8,24 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public class ResponseEntityGenerator {
-    public static <D> ResponseEntity<SuccessBody<D>> success(D data, HttpStatus status) {
-        return new ResponseEntity<>(
-                new SuccessBody<>(data), status);
+    public static <D> ResponseEntity<D> success(D data, HttpStatus status) {
+        return new ResponseEntity<>(data, status);
     }
 
-    public static ResponseEntity<SuccessBody<Void>> success(HttpStatus status) {
+    public static ResponseEntity<Void> success(HttpStatus status) {
         return success(null, status);
     }
 
-//    public static ResponseEntity<Object> fail(ErrorCode errorCode, List<ValidationError> invalidParams) {
-//        return ResponseEntity
-//                .status(errorCode.getHttpStatus())
-//                .body(new ErrorResponse(errorCode, invalidParams));
-//    }
-//
-//    public static ResponseEntity<ErrorResponse> fail(ErrorCode errorCode) {
-//        return fail(errorCode, null);
-//    }
     public static ResponseEntity<ErrorResponse> fail(ErrorCode errorCode) {
         return fail(errorCode, null);
     }
