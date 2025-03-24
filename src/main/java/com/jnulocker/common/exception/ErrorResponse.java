5d6abac2e34e3ac18d619d421f9ff3649ceaf443
Profile-exe <kang67572346@gmail.com>
2025-03-24 12:39:04 +0900

    feat: 비즈니스 예외에 대한 응답에 사용할 클래스 구현

---
+++ src/main/java/com/jnulocker/common/exception/ErrorResponse.java
@@ -0,0 +1,23 @@
+package com.jnulocker.common.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.util.List;
+
+public record ErrorResponse(
+        String code,
+        Integer status,
+        String message,
+        @JsonInclude(JsonInclude.Include.NON_NULL) List<ValidationError> invalidParams) {
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this(errorCode, null);
+    }
+
+    public ErrorResponse(ErrorCode errorCode, List<ValidationError> invalidParams) {
+        this(
+                errorCode.getCode(),
+                errorCode.getHttpStatus().value(),
+                errorCode.getMessage(),
+                invalidParams);
+    }
+}
