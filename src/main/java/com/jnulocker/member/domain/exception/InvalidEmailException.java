30af56810df7342ed18a4b08232eb6311c8c293b
westzeroright <kt8267@naver.com>
2025-03-29 00:03:11 +0900

    refactor: 예외 클래스 컨벤션 맞추기
    상위 클래스인 BusinessException을 처리하며 확정성 있게 다루도록 함

--- src/main/java/com/jnulocker/member/domain/exception/InvalidEmailException.java
+++ src/main/java/com/jnulocker/member/domain/exception/InvalidEmailException.java
@@ -3,7 +3,7 @@ package com.jnulocker.member.domain.exception;
 import com.jnulocker.common.exception.BusinessException;
 
 public class InvalidEmailException extends BusinessException {
-    public static final InvalidEmailException EXCEPTION = new InvalidEmailException();
+    public static final BusinessException EXCEPTION = new InvalidEmailException();
 
     public InvalidEmailException() {
         super(AuthErrorCode.INVALID_PASSWORD);
--- src/main/java/com/jnulocker/member/domain/exception/InvalidPasswordException.java
+++ src/main/java/com/jnulocker/member/domain/exception/InvalidPasswordException.java
@@ -3,7 +3,7 @@ package com.jnulocker.member.domain.exception;
 import com.jnulocker.common.exception.BusinessException;
 
 public class InvalidPasswordException extends BusinessException {
-    public static final InvalidPasswordException EXCEPTION = new InvalidPasswordException();
+    public static final BusinessException EXCEPTION = new InvalidPasswordException();
 
     private InvalidPasswordException() {
         super(AuthErrorCode.INVALID_EMAIL);
