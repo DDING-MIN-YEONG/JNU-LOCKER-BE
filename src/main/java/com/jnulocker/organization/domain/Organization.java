bc5bf5675ba669d671eb6a46ddfdc315808e2482
westzeroright <kt8267@naver.com>
2025-04-03 23:48:19 +0900

    feat: Organization 모델 작성

---
+++ src/main/java/com/jnulocker/organization/domain/Organization.java
@@ -0,0 +1,52 @@
+package com.jnulocker.organization.domain;
+
+import com.jnulocker.common.persistence.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Organization extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 공통연락망1
+    @Column(name = "email")
+    private String email;
+
+    // 공통연락망2
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "affiliation", nullable = false)
+    private String affiliatiion;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private Type type;
+
+    public void updateEmail(String email) {
+        this.email = email;
+    }
+
+    public void updatePhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+}
