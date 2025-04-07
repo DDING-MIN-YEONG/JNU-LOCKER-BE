86f7e178d2d2a52c28b9fab032347e4ea13f60a7
Profile-exe <kang67572346@gmail.com>
2025-04-07 10:48:14 +0900

    feat: Organization 일부 내용을 Department로 분리
    - name: 단과대/위원회 명칭
    - 나머지 공통 연락망, 학과명 및 별칭을 Department로 분리

--- src/main/java/com/jnulocker/organization/domain/Organization.java
+++ src/main/java/com/jnulocker/organization/domain/Organization.java
@@ -24,32 +24,14 @@ public class Organization extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 공통연락망1
-    @Column private String email;
-
-    // 공통연락망2
-    @Column private String phoneNumber;
+    @Column(nullable = false)
+    private String name;
 
     @Column(nullable = false)
-    private String affiliation;
-
-    @Column private String department;
-
-    @Column private String name;
-
-    @Column
     @Enumerated(EnumType.STRING)
     private OrganizationType type;
 
-    public void updateEmail(String email) {
-        this.email = email;
-    }
-
-    public void updatePhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void updateName(String name) {
-        this.name = name;
+    public static Organization create(String name, OrganizationType type) {
+        return Organization.builder().name(name).type(type).build();
     }
 }
