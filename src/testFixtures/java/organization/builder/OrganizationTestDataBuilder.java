1cae3a4fecf49305c99373666bbd17a64648a2d4
Profile-exe <kang67572346@gmail.com>
2025-04-07 11:06:07 +0900

    feat: Organization, Department 엔티티의 TestDataBuilder 구현

--- src/testFixtures/java/organization/builder/OrganizationTestDataBuilder.java
+++ src/testFixtures/java/organization/builder/OrganizationTestDataBuilder.java
@@ -1,14 +1,12 @@
 package organization.builder;
 
 import com.jnulocker.organization.domain.Organization;
+import com.jnulocker.organization.domain.OrganizationType;
 
 public class OrganizationTestDataBuilder {
 
-    private String email = "test@example.com";
-    private String phoneNumber = "010-1234-5678";
-    private String affiliation = "테스트 소속";
-    private String department = "테스트 학과";
     private String name = "테스트 조직명";
+    private OrganizationType type = OrganizationType.COUNCIL;
 
     private OrganizationTestDataBuilder() {}
 
@@ -16,32 +14,17 @@ public class OrganizationTestDataBuilder {
         return new OrganizationTestDataBuilder();
     }
 
-    public OrganizationTestDataBuilder withEmail(String email) {
-        this.email = email;
-        return this;
-    }
-
-    public OrganizationTestDataBuilder withPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-        return this;
-    }
-
-    public OrganizationTestDataBuilder withAffiliation(String affiliation) {
-        this.affiliation = affiliation;
-        return this;
-    }
-
-    public OrganizationTestDataBuilder withDepartment(String department) {
-        this.department = department;
-        return this;
-    }
-
     public OrganizationTestDataBuilder withName(String name) {
         this.name = name;
         return this;
     }
 
+    public OrganizationTestDataBuilder withType(OrganizationType type) {
+        this.type = type;
+        return this;
+    }
+
     public Organization build() {
-        return Organization.create(email, phoneNumber, affiliation, department, name);
+        return Organization.create(name, type);
     }
 }
