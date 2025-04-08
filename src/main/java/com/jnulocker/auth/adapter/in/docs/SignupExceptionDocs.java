5690e1811821878326c3d4ca52955aaf47953b05
westzeroright <kt8267@naver.com>
2025-04-08 20:13:52 +0900

    docs: 중복회원가입 상황 구체화

--- src/main/java/com/jnulocker/auth/adapter/in/docs/SignupExceptionDocs.java
+++ src/main/java/com/jnulocker/auth/adapter/in/docs/SignupExceptionDocs.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 public class SignupExceptionDocs implements SwaggerExceptionDoc {
 
     @ExplainError("동일한 메일에 대한 계정이 이미 존재할 때 발생하는 예외입니다.")
-    public static final BusinessException 동일계정이_존재할_때 = UserAlreadyExistException.EXCEPTION;
+    public static final BusinessException 동일한_메일을_사용하는_계정이_존재할_때 = UserAlreadyExistException.EXCEPTION;
 
     @ExplainError("단과대학 혹은 위원회가 존재하지 않을 때 발생하는 예외입니다.")
     public static final BusinessException 단과대학_혹은_위원회가_존재하지_않을_때 =
