b7435b16db6b05d639efe3b371df4eafc8c9a0cf
westzeroright <kt8267@naver.com>
2025-04-08 14:31:36 +0900

    refactor: 좀더 적절한 상태코드 전달

--- src/main/java/com/jnulocker/auth/adapter/in/docs/SignupExceptionDocs.java
+++ src/main/java/com/jnulocker/auth/adapter/in/docs/SignupExceptionDocs.java
@@ -17,8 +17,9 @@ public class SignupExceptionDocs implements SwaggerExceptionDoc {
     @ExplainError("동일한 메일에 대한 계정이 이미 존재할 때 발생하는 예외입니다.")
     public static final BusinessException 동일계정이_존재할_때 = UserAlreadyExistException.EXCEPTION;
 
-    @ExplainError("단과대학 혹은 위원회가 존재하지 않을 때 발생하는 예외입니다")
-    public static final BusinessException 단과대학_혹은_위원회가_존재하지_않을_때 = OrganizationNotFoundException.EXCEPTION;
+    @ExplainError("단과대학 혹은 위원회가 존재하지 않을 때 발생하는 예외입니다.")
+    public static final BusinessException 단과대학_혹은_위원회가_존재하지_않을_때 =
+            OrganizationNotFoundException.EXCEPTION;
 
     @ExplainError("요청시 보낸 권한에 대한 접근이 부적절할 때 발생하는 예외입니다.")
     public static final BusinessException 권한에_대한_접근_부적절 = RoleNotCorrectException.EXCEPTION;
--- src/main/java/com/jnulocker/auth/exception/AuthErrorCode.java
+++ src/main/java/com/jnulocker/auth/exception/AuthErrorCode.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
-    USER_ALREADY_EXIST("A001", HttpStatus.BAD_REQUEST, "이미 동일한 메일을 사용하는 계정이 존재합니다."),
-    ROLE_NOT_CORRECT("A002", HttpStatus.BAD_REQUEST, "요청한 권한에 대한 올바른 접근이 아닙니다.");
+    USER_ALREADY_EXIST("A001", HttpStatus.CONFLICT, "이미 동일한 메일을 사용하는 계정이 존재합니다."),
+    ROLE_NOT_CORRECT("A002", HttpStatus.BAD_REQUEST, "요청한 권한에 대한 올바른 접근이 아닙니다."),
+    ;
 
     private final String code;
     private final HttpStatus httpStatus;
