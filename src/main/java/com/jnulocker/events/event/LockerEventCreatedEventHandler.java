ed7af9c43677ba31c0641c573e8424b9ba9f69ee
Profile-exe <kang67572346@gmail.com>
2025-04-08 21:48:47 +0900

    feat: 이벤트 생성 시 Job을 스케줄러에 등록하는 이벤트 핸들러 구현

---
+++ src/main/java/com/jnulocker/events/event/LockerEventCreatedEventHandler.java
@@ -0,0 +1,84 @@
+package com.jnulocker.events.event;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+import com.jnulocker.events.quartz.job.EventCloseJob;
+import com.jnulocker.events.quartz.job.EventOpenJob;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class LockerEventCreatedEventHandler {
+
+    private static final String OPEN_JOB_NAME = "EVENT_OPEN_JOB-";
+    private static final String OPEN_TRIGGER_NAME = "EVENT_OPEN_TRIGGER-";
+
+    private static final String CLOSE_JOB_NAME = "EVENT_CLOSE_JOB-";
+    private static final String CLOSE_TRIGGER_NAME = "EVENT_CLOSE_TRIGGER-";
+
+    private static final String EVENT_JOB_GROUP = "EVENT_JOB_GROUP";
+    private static final String EVENT_TRIGGER_GROUP = "EVENT_TRIGGER_GROUP";
+
+    private static final String EVENT_ID = "eventId";
+    private static final String ASIA_SEOUL = "Asia/Seoul";
+
+    private final Scheduler scheduler;
+
+    @TransactionalEventListener(
+            classes = LockerEventCreatedEvent.class,
+            phase = TransactionPhase.BEFORE_COMMIT)
+    public void handle(LockerEventCreatedEvent lockerEventCreatedEvent) throws SchedulerException {
+
+        Long eventId = lockerEventCreatedEvent.getEventId();
+
+        scheduleEventJob( // Event Open Job 등록
+                lockerEventCreatedEvent.getStartAt(),
+                newJob(EventOpenJob.class),
+                OPEN_JOB_NAME,
+                eventId,
+                OPEN_TRIGGER_NAME);
+
+        scheduleEventJob( // Event Close Job 등록
+                lockerEventCreatedEvent.getEndAt(),
+                newJob(EventCloseJob.class),
+                CLOSE_JOB_NAME,
+                eventId,
+                CLOSE_TRIGGER_NAME);
+    }
+
+    private void scheduleEventJob(
+            LocalDateTime localDateTime,
+            JobBuilder newJob,
+            String jobName,
+            Long eventId,
+            String triggerName)
+            throws SchedulerException {
+        Date startAt = Date.from(localDateTime.atZone(ZoneId.of(ASIA_SEOUL)).toInstant());
+
+        JobDetail jobDetail =
+                newJob.withIdentity(jobName + eventId, EVENT_JOB_GROUP)
+                        .usingJobData(EVENT_ID, eventId)
+                        .build();
+
+        Trigger eventOpenTrigger =
+                newTrigger()
+                        .withIdentity(triggerName + eventId, EVENT_TRIGGER_GROUP)
+                        .forJob(jobDetail)
+                        .startAt(startAt)
+                        .build();
+
+        scheduler.scheduleJob(jobDetail, eventOpenTrigger);
+    }
+}
