81133319a5aeddd6e959d9cb1904d169aef0cef7
westzeroright <kt8267@naver.com>
2025-04-08 20:04:22 +0900

    refactor: 학과가 존재하지 않는 예외

--- src/main/java/com/jnulocker/organization/exception/DepartmentErrorCode.java
+++ src/main/java/com/jnulocker/organization/exception/DepartmentErrorCode.java
@@ -7,9 +7,8 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum OrganizationErrorCode implements ErrorCode {
-    ORGANIZATION_NOT_FOUND("O001", HttpStatus.NOT_FOUND, "단과대학 혹은 위원회가 존재하지 않습니다"),
-    INVALID_ORGANIZATION("O002", HttpStatus.BAD_REQUEST, "잘못된 접근입니다."),
+public enum DepartmentErrorCode implements ErrorCode {
+    DEPARTMENT_NOT_FOUND("D001", HttpStatus.NOT_FOUND, "학과가 존재하지 않습니다"),
     ;
 
     private final String code;
--- src/main/java/com/jnulocker/organization/exception/DepartmentNotFoundException.java
+++ src/main/java/com/jnulocker/organization/exception/DepartmentNotFoundException.java
@@ -3,9 +3,10 @@ package com.jnulocker.organization.exception;
 import com.jnulocker.common.exception.BusinessException;
 
 public class DepartmentNotFoundException extends BusinessException {
+
     public static final BusinessException EXCEPTION = new DepartmentNotFoundException();
 
     private DepartmentNotFoundException() {
-        super(OrganizationErrorCode.ORGANIZATION_NOT_FOUND);
+        super(DepartmentErrorCode.DEPARTMENT_NOT_FOUND);
     }
 }
