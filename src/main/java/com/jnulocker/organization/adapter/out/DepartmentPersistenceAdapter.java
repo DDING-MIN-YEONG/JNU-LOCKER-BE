c0727c63bac588d73174d113c7631f7faf2a108f
Profile-exe <kang67572346@gmail.com>
2025-04-08 18:33:03 +0900

    feat: Department 조회를 위한 LoadPort 메서드 정의 및 구현

--- src/main/java/com/jnulocker/organization/adapter/out/DepartmentPersistenceAdapter.java
+++ src/main/java/com/jnulocker/organization/adapter/out/DepartmentPersistenceAdapter.java
@@ -4,6 +4,7 @@ import com.jnulocker.common.annotation.PersistenceAdapter;
 import com.jnulocker.organization.application.port.out.DepartmentLoadPort;
 import com.jnulocker.organization.domain.Department;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 
 @PersistenceAdapter
@@ -15,5 +16,15 @@ public class DepartmentPersistenceAdapter implements DepartmentLoadPort {
     @Override
     public List<Department> getDepartmentsByOrganizationId(Long organizationId) {
         return departmentRepository.findAllByOrganizationId(organizationId);
+    }
+
+    @Override
+    public Optional<Department> getDepartmentById(Long departmentId) {
+        return departmentRepository.findById(departmentId);
+    }
+
+    @Override
+    public List<Department> getDepartmentsByIdIn(List<Long> departmentIds) {
+        return departmentRepository.findAllByIdIn(departmentIds);
     }
 }
--- src/main/java/com/jnulocker/organization/application/port/out/DepartmentLoadPort.java
+++ src/main/java/com/jnulocker/organization/application/port/out/DepartmentLoadPort.java
@@ -2,8 +2,13 @@ package com.jnulocker.organization.application.port.out;
 
 import com.jnulocker.organization.domain.Department;
 import java.util.List;
+import java.util.Optional;
 
 public interface DepartmentLoadPort {
 
     List<Department> getDepartmentsByOrganizationId(Long organizationId);
+
+    Optional<Department> getDepartmentById(Long departmentId);
+
+    List<Department> getDepartmentsByIdIn(List<Long> departmentIds);
 }
