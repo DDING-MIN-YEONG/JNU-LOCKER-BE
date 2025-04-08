a5d1287f41c0e7b200c9f6e6bf1663e90fd3cdb4
westzeroright <kt8267@naver.com>
2025-04-08 20:00:36 +0900

    refacotr: AuthService에서 port가 아닌 command/query를 주입받음

--- src/main/java/com/jnulocker/auth/application/service/AuthService.java
+++ src/main/java/com/jnulocker/auth/application/service/AuthService.java
@@ -2,17 +2,14 @@ package com.jnulocker.auth.application.service;
 
 import com.jnulocker.auth.application.port.in.UserSignupCommand;
 import com.jnulocker.auth.application.port.in.request.UserSignupRequest;
-import com.jnulocker.auth.application.port.out.MemberRecordPort;
 import com.jnulocker.auth.exception.RoleNotCorrectException;
 import com.jnulocker.auth.exception.UserAlreadyExistException;
-import com.jnulocker.member.application.port.out.MemberLoadPort;
+import com.jnulocker.member.application.port.in.MemberCommand;
+import com.jnulocker.member.application.port.in.MemberQuery;
 import com.jnulocker.member.domain.Member;
 import com.jnulocker.member.domain.Role;
-import com.jnulocker.organization.application.port.out.DepartmentLoadPort;
-import com.jnulocker.organization.application.port.out.OrganizationLoadPort;
+import com.jnulocker.organization.application.port.in.GetDepartmentQuery;
 import com.jnulocker.organization.domain.Department;
-import com.jnulocker.organization.domain.Organization;
-import com.jnulocker.organization.exception.InvalidOrganizationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,42 +18,30 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class AuthService implements UserSignupCommand {
-    private final MemberLoadPort memberLoadPort;
     private final PasswordEncoder passwordEncoder;
-    private final MemberRecordPort memberRecordPort;
-    private final OrganizationLoadPort organizationLoadPort;
-    private final DepartmentLoadPort departmentLoadPort;
+    private final MemberQuery memberQuery;
+    private final MemberCommand memberCommand;
+    private final GetDepartmentQuery getDepartmentQuery;
 
     @Override
     @Transactional
-    public void signupUser(UserSignupRequest userSignupRequest) {
-        if (memberLoadPort.existsByEmail(userSignupRequest.email())) {
+    public void signupUser(UserSignupRequest request) {
+        if (memberQuery.existsByEmail(request.email())) {
             throw UserAlreadyExistException.EXCEPTION;
         }
 
-        if (!userSignupRequest.role().equals(Role.USER)) {
+        if (!request.role().equals(Role.USER)) {
             throw RoleNotCorrectException.EXCEPTION;
         }
 
-        String encodedPassword = passwordEncoder.encode(userSignupRequest.password());
+        String encodedPassword = passwordEncoder.encode(request.password());
 
-        Department department = departmentLoadPort.getById(userSignupRequest.departmentId());
-        validOrganization(userSignupRequest.organizationId(), department.getOrganization());
+        Department department = getDepartmentQuery.getDepartmentByIdOrThrow(request.departmentId());
 
         Member member =
                 Member.createUser(
-                        userSignupRequest.email(),
-                        encodedPassword,
-                        userSignupRequest.phoneNumber(),
-                        department);
+                        request.email(), encodedPassword, request.phoneNumber(), department);
 
-        memberRecordPort.save(member);
-    }
-
-    public void validOrganization(Long organizationId, Organization organization) {
-        Organization foundOrgan = organizationLoadPort.getById(organizationId);
-        if (foundOrgan != organization) {
-            throw InvalidOrganizationException.EXCEPTION;
-        }
+        memberCommand.save(member);
     }
 }
