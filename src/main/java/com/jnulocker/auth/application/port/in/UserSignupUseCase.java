6e9fd815b8e7143677b8acb2e0676742f02c316e
westzeroright <kt8267@naver.com>
2025-04-08 14:34:03 +0900

    refactor: 타입과 변수명 통일

--- src/main/java/com/jnulocker/auth/application/port/in/UserSignupUseCase.java
+++ src/main/java/com/jnulocker/auth/application/port/in/UserSignupUseCase.java
@@ -3,5 +3,5 @@ package com.jnulocker.auth.application.port.in;
 import com.jnulocker.auth.application.port.in.request.UserSignupRequest;
 
 public interface UserSignupUseCase {
-    void signupUser(UserSignupRequest userSignupReqDto);
+    void signupUser(UserSignupRequest userSignupRequest);
 }
