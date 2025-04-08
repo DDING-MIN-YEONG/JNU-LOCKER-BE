31197a80680c8d5dab78a3db76d66f78c7c01766
westzeroright <kt8267@naver.com>
2025-04-08 14:06:13 +0900

    refactor: 예외 문서 통일

--- src/main/java/com/jnulocker/auth/adapter/in/docs/SignupExceptionDocs.java
+++ src/main/java/com/jnulocker/auth/adapter/in/docs/SignupExceptionDocs.java
@@ -17,8 +17,8 @@ public class SignupExceptionDocs implements SwaggerExceptionDoc {
     @ExplainError("동일한 메일에 대한 계정이 이미 존재할 때 발생하는 예외입니다.")
     public static final BusinessException 동일계정이_존재할_때 = UserAlreadyExistException.EXCEPTION;
 
-    @ExplainError("회원가입시 입력받은 소속대학/소속학과와 일치한 정보를 찾지 못할 때 발생하는 예외입니다.")
-    public static final BusinessException 소속대학_소속학과_불일치 = OrganizationNotFoundException.EXCEPTION;
+    @ExplainError("단과대학 혹은 위원회가 존재하지 않을 때 발생하는 예외입니다")
+    public static final BusinessException 단과대학_혹은_위원회가_존재하지_않을_때 = OrganizationNotFoundException.EXCEPTION;
 
     @ExplainError("요청시 보낸 권한에 대한 접근이 부적절할 때 발생하는 예외입니다.")
     public static final BusinessException 권한에_대한_접근_부적절 = RoleNotCorrectException.EXCEPTION;
