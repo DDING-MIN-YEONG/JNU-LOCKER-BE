e2c7e9ee8d29cc60cdaff8d9f2eee42015d00360
westzeroright <kt8267@naver.com>
2025-04-10 00:13:48 +0900

    refactor: 회원가입예외에서 소속대학미조회예외 제거

--- src/main/java/com/jnulocker/auth/adapter/in/docs/SignupExceptionDocs.java
+++ src/main/java/com/jnulocker/auth/adapter/in/docs/SignupExceptionDocs.java
@@ -6,7 +6,6 @@ import com.jnulocker.common.swagger.ExceptionDoc;
 import com.jnulocker.common.swagger.ExplainError;
 import com.jnulocker.common.swagger.SwaggerExceptionDoc;
 import com.jnulocker.organization.exception.DepartmentNotFoundException;
-import com.jnulocker.organization.exception.OrganizationNotFoundException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -17,10 +16,6 @@ public class SignupExceptionDocs implements SwaggerExceptionDoc {
     @ExplainError("동일한 메일에 대한 계정이 이미 존재할 때 발생하는 예외입니다.")
     public static final BusinessException 동일한_메일을_사용하는_계정이_존재할_때 =
             UserAlreadyExistException.EXCEPTION;
-
-    @ExplainError("단과대학 혹은 위원회가 존재하지 않을 때 발생하는 예외입니다.")
-    public static final BusinessException 단과대학_혹은_위원회가_존재하지_않을_때 =
-            OrganizationNotFoundException.EXCEPTION;
 
     @ExplainError("학과가 존재하지 않을 때 발생하는 예외입니다.")
     public static final BusinessException 학과가_존재하지_않을_때 = DepartmentNotFoundException.EXCEPTION;
