d8eace4d1df0b2cb2b869699e1362f384d902d0c
Profile-exe <kang67572346@gmail.com>
2025-04-09 14:43:55 +0900

    feat: 이벤트 목록 조회 Query 정의 및 구현 및 메서드 위치 변경
    - 이벤트 조회 관련 쿼리가 위로 사물함 조회 관련 쿼리가 아래로 위치시킴

--- src/main/java/com/jnulocker/events/application/port/in/EventQuery.java
+++ src/main/java/com/jnulocker/events/application/port/in/EventQuery.java
@@ -1,12 +1,16 @@
 package com.jnulocker.events.application.port.in;
 
+import com.jnulocker.events.application.port.in.response.EventCustomPage;
 import com.jnulocker.events.application.port.in.response.FloorWithLockersResponse;
 import com.jnulocker.events.domain.Event;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface EventQuery {
 
-    List<FloorWithLockersResponse> getLockersByEventId(Long eventId);
-
     Event getByIdOrThrow(Long eventId);
+
+    EventCustomPage getAllEvents(Pageable pageable);
+
+    List<FloorWithLockersResponse> getLockersByEventId(Long eventId);
 }
--- src/main/java/com/jnulocker/events/application/service/EventQueryService.java
+++ src/main/java/com/jnulocker/events/application/service/EventQueryService.java
@@ -1,6 +1,7 @@
 package com.jnulocker.events.application.service;
 
 import com.jnulocker.events.application.port.in.EventQuery;
+import com.jnulocker.events.application.port.in.response.EventCustomPage;
 import com.jnulocker.events.application.port.in.response.FloorWithLockersResponse;
 import com.jnulocker.events.application.port.in.response.LockerResponse;
 import com.jnulocker.events.application.port.out.EventLoadPort;
@@ -11,6 +12,8 @@ import com.jnulocker.events.domain.Floor;
 import com.jnulocker.events.exception.EventNotFoundException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,6 +23,18 @@ public class EventQueryService implements EventQuery {
     private final EventLoadPort eventLoadPort;
     private final FloorLoadPort floorLoadPort;
     private final LockerLoadPort lockerLoadPort;
+
+    @Override
+    public Event getByIdOrThrow(Long eventId) {
+        return eventLoadPort.getById(eventId).orElseThrow(() -> EventNotFoundException.EXCEPTION);
+    }
+
+    @Override
+    public EventCustomPage getAllEvents(Pageable pageable) {
+        // TODO: MANAGER가 조회하는 경우 자신이 소속된 조직의 이벤트만 조회할 수 있도록 수정
+        Page<Event> events = eventLoadPort.getAllEvents(pageable);
+        return EventCustomPage.from(events);
+    }
 
     @Override
     public List<FloorWithLockersResponse> getLockersByEventId(Long eventId) {
@@ -43,10 +58,5 @@ public class EventQueryService implements EventQuery {
         return lockerLoadPort.getLockersByFloorId(floor.getId()).stream()
                 .map(LockerResponse::from)
                 .toList();
-    }
-
-    @Override
-    public Event getByIdOrThrow(Long eventId) {
-        return eventLoadPort.getById(eventId).orElseThrow(() -> EventNotFoundException.EXCEPTION);
     }
 }
