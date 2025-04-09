5e8728280cf9e9ee928967f779046233fe02d9bf
Profile-exe <kang67572346@gmail.com>
2025-04-09 14:35:42 +0900

    feat: 이벤트 목록 조회 Port 정의 및 구현

--- src/main/java/com/jnulocker/events/application/port/out/EventLoadPort.java
+++ src/main/java/com/jnulocker/events/application/port/out/EventLoadPort.java
@@ -2,10 +2,14 @@ package com.jnulocker.events.application.port.out;
 
 import com.jnulocker.events.domain.Event;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface EventLoadPort {
 
     boolean existsById(Long eventId);
 
     Optional<Event> getById(Long eventId);
+
+    Page<Event> getAllEvents(Pageable pageable);
 }
