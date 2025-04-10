d1e0ef06df7ad4c4f3fe91276af51ab9606ba570
westzeroright <kt8267@naver.com>
2025-04-10 12:50:11 +0900

    chore: cors에 개발서버 주소 추가

--- src/main/java/com/jnulocker/config/WebConfig.java
+++ src/main/java/com/jnulocker/config/WebConfig.java
@@ -12,8 +12,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) { // CORS 설정
         registry.addMapping("/**")
                 .allowedOriginPatterns(
-                        "http://localhost:3000", "https://localhost:3000",
-                        "http://localhost:8080", "https://localhost:8080")
+                        "http://localhost:3000",
+                        "https://localhost:3000",
+                        "http://localhost:8080",
+                        "https://localhost:8080",
+                        "https://api.dev.jnu-locker.site")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
