1eafbde92005306641f01c3503906929e1b6b807
westzeroright <kt8267@naver.com>
2025-04-24 23:54:05 +0900

    refactor: 인가 예외 응답 커스텀

--- src/main/java/com/jnulocker/auth/exception/AuthErrorCode.java
+++ src/main/java/com/jnulocker/auth/exception/AuthErrorCode.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum AuthErrorCode implements ErrorCode {
     USER_ALREADY_EXIST("A001", HttpStatus.CONFLICT, "이미 동일한 메일을 사용하는 계정이 존재합니다."),
     FAIL_AUTHENTICATION("A002", HttpStatus.UNAUTHORIZED, "인증에 실패했습니다."),
+    FAIL_AUTHORIZATION("A003", HttpStatus.FORBIDDEN, "권한이 없습니다."),
     ;
 
     private final String code;
--- src/main/java/com/jnulocker/auth/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/jnulocker/auth/security/CustomAccessDeniedHandler.java
@@ -1,5 +1,9 @@
 package com.jnulocker.auth.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jnulocker.auth.exception.AuthErrorCode;
+import com.jnulocker.common.exception.ErrorCode;
+import com.jnulocker.common.exception.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,14 +17,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
+    private static final String MEDIA_TYPE = "application/json; charset=UTF-8";
+
     @Override
     public void handle(
             HttpServletRequest request,
             HttpServletResponse response,
             AccessDeniedException accessDeniedException)
             throws IOException {
-        response.setContentType("application/json;charset=UTF-8");
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-        response.getWriter().write("{\"error\":\"접근 권한이 없습니다.\",\"status\":403}");
+        ErrorCode errorCode = AuthErrorCode.FAIL_AUTHORIZATION;
+        ErrorResponse errorResponse = new ErrorResponse(errorCode);
+
+        response.setStatus(errorCode.getHttpStatus().value());
+        response.setContentType(MEDIA_TYPE);
+
+        String jsonResponse = new ObjectMapper().writeValueAsString(errorResponse);
+        response.getWriter().write(jsonResponse);
     }
 }
