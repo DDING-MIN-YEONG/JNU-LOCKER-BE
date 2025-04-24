fa6add5eee87851bb838b545ec250faede1631ab
westzeroright <kt8267@naver.com>
2025-04-24 22:56:00 +0900

    style: spotless

--- src/main/java/com/jnulocker/auth/exception/FailAuthenticationException.java
+++ src/main/java/com/jnulocker/auth/exception/FailAuthenticationException.java
@@ -4,6 +4,7 @@ import com.jnulocker.common.exception.BusinessException;
 
 public class FailAuthenticationException extends BusinessException {
     public static final BusinessException EXCEPTION = new FailAuthenticationException();
+
     private FailAuthenticationException() {
         super(AuthErrorCode.FAIL_AUTHENTICATION);
     }
