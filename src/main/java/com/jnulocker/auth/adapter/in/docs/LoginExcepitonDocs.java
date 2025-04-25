1722632f2d17aa7246c3413d9530413c6b3e25b6
westzeroright <kt8267@naver.com>
2025-04-25 14:41:01 +0900

    docs: reissue API 예외 문서 작성

--- src/main/java/com/jnulocker/auth/adapter/in/docs/LoginExcepitonDocs.java
+++ src/main/java/com/jnulocker/auth/adapter/in/docs/LoginExcepitonDocs.java
@@ -1,5 +1,6 @@
 package com.jnulocker.auth.adapter.in.docs;
 
+import com.jnulocker.auth.exception.FailAuthenticationException;
 import com.jnulocker.common.exception.BusinessException;
 import com.jnulocker.common.swagger.ExceptionDoc;
 import com.jnulocker.common.swagger.ExplainError;
@@ -11,7 +12,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class LoginExcepitonDocs implements SwaggerExceptionDoc {
 
-    @ExplainError("이메일/비밀번호가 올바르지 않을 때 발생한느 예외입니다.")
-    public static final BusinessException 인증에_실패했을_때 =
-            com.jnulocker.auth.exception.FailAuthenticationException.EXCEPTION;
+    @ExplainError("이메일/비밀번호가 올바르지 않을 때 발생하는 예외입니다.")
+    public static final BusinessException 인증에_실패했을_때 = FailAuthenticationException.EXCEPTION;
 }
