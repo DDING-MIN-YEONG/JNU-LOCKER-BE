0ea29aa6ed55ea2c4deb3dcbe85423d9bb31a8f5
westzeroright <kt8267@naver.com>
2025-04-25 16:03:01 +0900

    refactor: 응답을 JSON 형식으로 보내도록 수정
    - 인증 관련 예외는 EntryPoint에서 중앙관리되고 있으나
    - JwtFilter에서 reqeust.setAttribute로 설정한 예외를 EntryPoint에서 못 받을 수 있으므로
    - 응답 구조 형식 코드가 필요함
    - 이를 EntryPoint와 동일하게 함

--- src/main/java/com/jnulocker/auth/jwt/JwtFilter.java
+++ src/main/java/com/jnulocker/auth/jwt/JwtFilter.java
@@ -2,8 +2,12 @@ package com.jnulocker.auth.jwt;
 
 import static com.jnulocker.auth.util.CookieUtil.getCookieValueFromAccessToken;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jnulocker.auth.exception.AuthErrorCode;
 import com.jnulocker.auth.jwt.exception.InvalidAccessTokenException;
 import com.jnulocker.common.exception.BusinessException;
+import com.jnulocker.common.exception.ErrorCode;
+import com.jnulocker.common.exception.ErrorResponse;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -60,11 +64,21 @@ public class JwtFilter extends OncePerRequestFilter {
         } catch (BusinessException e) {
             log.error("JWT 처리 중 예외 발생: {}", e.getMessage());
             request.setAttribute("exception", e);
-            response.sendError(e.getErrorCode().getHttpStatus().value(), e.getMessage());
+            setErrorResponse(response, e.getErrorCode());
         } catch (Exception e) {
             log.error("JWT 처리 중 예기치 않은 예외 발생: {}", e.getMessage());
             request.setAttribute("exception", e);
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증 처리 중 오류가 발생했습니다.");
+            setErrorResponse(response, AuthErrorCode.FAIL_AUTHENTICATION);
         }
+    }
+
+    private void setErrorResponse(HttpServletResponse response, ErrorCode errorCode)
+            throws IOException {
+        response.setStatus(errorCode.getHttpStatus().value());
+        response.setContentType("application/json;charset=UTF-8");
+
+        ErrorResponse errorResponse = new ErrorResponse(errorCode);
+        String jsonResponse = new ObjectMapper().writeValueAsString(errorResponse);
+        response.getWriter().write(jsonResponse);
     }
 }
