f8ff45be7cc3667ac081599a490365a53ec36aac
westzeroright <kt8267@naver.com>
2025-04-26 00:36:37 +0900

    refactor: LoginRequest 이메일 형식 검증 응답 반환 메시지 지정

--- src/main/java/com/jnulocker/auth/application/port/in/request/LoginRequest.java
+++ src/main/java/com/jnulocker/auth/application/port/in/request/LoginRequest.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.NotBlank;
 public record LoginRequest(
         @Schema(description = "이메일", example = "abcde@example.com")
                 @NotBlank(message = "이메일은 필수 입력값입니다")
-                @Email
+                @Email(message = "올바른 형식의 이메일 주소여야 합니다")
                 String email,
         @Schema(description = "비밀번호", example = "abcde12345!")
                 @NotBlank(message = "비밀번호는 필수 입력값입니다")
