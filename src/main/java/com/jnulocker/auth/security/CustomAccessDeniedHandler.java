2f34ee10101700631e2096f185c6bebb1d27783d
westzeroright <kt8267@naver.com>
2025-04-25 17:05:10 +0900

    refactor: ObjectMapper를 new보다 주입받아서 사용

--- src/main/java/com/jnulocker/auth/security/CustomAccessDeniedHandler.java
+++ src/main/java/com/jnulocker/auth/security/CustomAccessDeniedHandler.java
@@ -7,6 +7,7 @@ import com.jnulocker.common.exception.ErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -15,9 +16,12 @@ import org.springframework.stereotype.Component;
 인가를 처리함 - 알맞지 않는 권한으로 요청을 보낼 시
  */
 @Component
+@RequiredArgsConstructor
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     private static final String MEDIA_TYPE = "application/json; charset=UTF-8";
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void handle(
@@ -31,7 +35,7 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
         response.setStatus(errorCode.getHttpStatus().value());
         response.setContentType(MEDIA_TYPE);
 
-        String jsonResponse = new ObjectMapper().writeValueAsString(errorResponse);
+        String jsonResponse = objectMapper.writeValueAsString(errorResponse);
         response.getWriter().write(jsonResponse);
     }
 }
