ebe498566f3a4c799d667c8e25f85f5bcb73cef5
Profile-exe <kang67572346@gmail.com>
2025-04-28 13:58:05 +0900

    feat: 사물함 신청 비즈니스 로직 구현

---
+++ src/main/java/com/jnulocker/registration/application/port/in/request/RegisterForEventRequest.java
@@ -0,0 +1,3 @@
+package com.jnulocker.registration.application.port.in.request;
+
+public record RegisterForEventRequest(Long lockerId) {}
---
+++ src/main/java/com/jnulocker/registration/application/service/RegistrationCommandService.java
@@ -0,0 +1,43 @@
+package com.jnulocker.registration.application.service;
+
+import com.jnulocker.auth.security.SecurityUtils;
+import com.jnulocker.events.application.port.in.EventQuery;
+import com.jnulocker.events.domain.Locker;
+import com.jnulocker.member.application.port.in.MemberQuery;
+import com.jnulocker.member.domain.Member;
+import com.jnulocker.registration.application.port.in.RegistrationCommand;
+import com.jnulocker.registration.application.port.in.request.RegisterForEventRequest;
+import com.jnulocker.registration.application.port.out.RegistrationLoadPort;
+import com.jnulocker.registration.application.port.out.RegistrationRecordPort;
+import com.jnulocker.registration.domain.Registration;
+import com.jnulocker.registration.exception.RegistrationAlreadyExistsException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class RegistrationCommandService implements RegistrationCommand {
+
+    private final MemberQuery memberQuery;
+    private final EventQuery eventQuery;
+    private final RegistrationLoadPort registrationLoadPort;
+    private final RegistrationRecordPort registrationRecordPort;
+
+    @Override
+    @Transactional
+    public void registerForEvent(Long eventId, RegisterForEventRequest request) {
+        Long memberId = SecurityUtils.getCurrentMemberId();
+        Member member = memberQuery.findByIdOrThrow(memberId);
+
+        // 이미 해당 이벤트에서 사물함 신청을 완료한 경우 예외 발생
+        if (registrationLoadPort.existsByMemberIdAndEventId(memberId, eventId)) {
+            throw RegistrationAlreadyExistsException.EXCEPTION;
+        }
+
+        Locker locker = eventQuery.getLockerByIdOrThrow(request.lockerId());
+
+        Registration registration = Registration.create(member, locker);
+        registrationRecordPort.save(registration);
+    }
+}
