8d76fac0af04af897736aaea9ef5dbac627f2037
Profile-exe <kang67572346@gmail.com>
2025-04-28 13:56:03 +0900

    feat: 사물함 신청 정보 조회 Port 구현

--- src/main/java/com/jnulocker/registration/adapter/out/RegistrationRepository.java
+++ src/main/java/com/jnulocker/registration/adapter/out/RegistrationRepository.java
@@ -3,4 +3,6 @@ package com.jnulocker.registration.adapter.out;
 import com.jnulocker.registration.domain.Registration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegistrationRepository extends JpaRepository<Registration, Long> {}
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+    boolean existsByMemberIdAndLocker_Floor_EventId(Long memberId, Long eventId);
+}
