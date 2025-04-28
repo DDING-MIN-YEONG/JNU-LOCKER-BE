a47b9c042f71c3be024898fd24b8f61b92467260
Profile-exe <kang67572346@gmail.com>
2025-04-28 18:31:39 +0900

    test: Member 엔티티에 대한 TestDataBuilder와 Util 클래스 구현

---
+++ src/testFixtures/java/member/domain/MemberTestDataBuilder.java
@@ -0,0 +1,54 @@
+package member.domain;
+
+import static organization.domain.DepartmentTestDataBuilder.departmentBuilder;
+
+import com.jnulocker.member.domain.Member;
+import com.jnulocker.organization.domain.Department;
+
+public class MemberTestDataBuilder {
+
+    private String name = "테스트 이름";
+    private String email = "test@email.com";
+    private String phoneNumber = "010-1234-5678";
+    private String password = "password123";
+    private Department department = departmentBuilder().build();
+
+    private MemberTestDataBuilder() {}
+
+    public static MemberTestDataBuilder memberBuilder() {
+        return new MemberTestDataBuilder();
+    }
+
+    public MemberTestDataBuilder withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public MemberTestDataBuilder withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public MemberTestDataBuilder withPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+
+    public MemberTestDataBuilder withPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public MemberTestDataBuilder withDepartment(Department department) {
+        this.department = department;
+        return this;
+    }
+
+    public Member buildUser() {
+        return Member.createUser(name, email, phoneNumber, password, department);
+    }
+
+    public Member buildManager() {
+        return Member.createManager(name, email, phoneNumber, password, department);
+    }
+}
