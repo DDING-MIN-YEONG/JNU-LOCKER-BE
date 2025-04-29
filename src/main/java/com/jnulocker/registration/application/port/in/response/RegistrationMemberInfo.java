7686e7d8b739c7782459161fbd719b3a37f2cc81
Profile-exe <kang67572346@gmail.com>
2025-04-29 22:49:26 +0900

    docs: 사물함 신청자 이메일 예시 학교 메일주소로 변경

--- src/main/java/com/jnulocker/registration/application/port/in/response/RegistrationMemberInfo.java
+++ src/main/java/com/jnulocker/registration/application/port/in/response/RegistrationMemberInfo.java
@@ -7,7 +7,7 @@ public record RegistrationMemberInfo(
         @Schema(description = "신청자 이름", example = "심민보") String name,
         @Schema(description = "신청자 학번", example = "222312") String studentNumber,
         @Schema(description = "신청자 소속학과", example = "컴퓨터정보통신공학과") String department,
-        @Schema(description = "신청자 이메일", example = "asdf123@example.com") String email) {
+        @Schema(description = "신청자 이메일", example = "asdf123@jnu.ac.kr") String email) {
     public static RegistrationMemberInfo from(Member member) {
         return new RegistrationMemberInfo(
                 member.getName(),
