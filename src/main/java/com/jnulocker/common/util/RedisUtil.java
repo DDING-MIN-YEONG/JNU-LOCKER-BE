faa9d3ac6f9c2685edb1acbe67e67d2c267bfed8
westzeroright <kt8267@naver.com>
2025-04-30 23:22:52 +0900

    refactor: RedisUtil에 @Component 적용

--- src/main/java/com/jnulocker/common/util/RedisUtil.java
+++ src/main/java/com/jnulocker/common/util/RedisUtil.java
@@ -4,9 +4,10 @@ import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class RedisUtil {
     private final StringRedisTemplate redisTemplate;
