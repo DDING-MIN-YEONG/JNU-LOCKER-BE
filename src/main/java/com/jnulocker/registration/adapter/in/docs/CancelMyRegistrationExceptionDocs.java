24739ec1bb80268bbc0b97865d5bc5f0fe87c86a
Profile-exe <kang67572346@gmail.com>
2025-04-30 20:00:39 +0900

    docs: 추가 예외 문서화

--- src/main/java/com/jnulocker/registration/adapter/in/docs/CancelMyRegistrationExceptionDocs.java
+++ src/main/java/com/jnulocker/registration/adapter/in/docs/CancelMyRegistrationExceptionDocs.java
@@ -5,6 +5,7 @@ import com.jnulocker.common.swagger.ExceptionDoc;
 import com.jnulocker.common.swagger.ExplainError;
 import com.jnulocker.common.swagger.SwaggerExceptionDoc;
 import com.jnulocker.events.exception.EventNotFoundException;
+import com.jnulocker.registration.exception.RegistrationNotFoundException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -14,4 +15,7 @@ public class CancelMyRegistrationExceptionDocs implements SwaggerExceptionDoc {
 
     @ExplainError("이벤트가 존재하지 않을 때 발생하는 예외입니다")
     public static final BusinessException 이벤트가_존재하지_않을_때 = EventNotFoundException.EXCEPTION;
+
+    @ExplainError("사물함 신청 내역이 존재하지 않을 때 발생하는 예외입니다")
+    public static final BusinessException 신청_내역이_존재하지_않을_때 = RegistrationNotFoundException.EXCEPTION;
 }
