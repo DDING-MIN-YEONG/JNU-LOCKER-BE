fff6e1fc3714db3e47c7c071ef32cd0bbe33afee
westzeroright <kt8267@naver.com>
2025-04-30 17:14:36 +0900

    feat: 요청 본문에서 인증 코드 형식에 대한 검증

--- src/main/java/com/jnulocker/auth/application/port/in/request/VerifyCodeRequest.java
+++ src/main/java/com/jnulocker/auth/application/port/in/request/VerifyCodeRequest.java
@@ -2,8 +2,11 @@ package com.jnulocker.auth.application.port.in.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record VerifyCodeRequest(
         @NotBlank(message = "이메일은 필수 입력 값입니다") @Email(message = "올바른 형식의 이메일 주소여야 합니다")
                 String email,
-        String code) {}
+        @NotBlank(message = "인증 코드는 필수 입력 값입니다")
+                @Pattern(regexp = "^[0-9]{6}$", message = "인증 코드는 6자리 숫자여야 합니다")
+                String code) {}
