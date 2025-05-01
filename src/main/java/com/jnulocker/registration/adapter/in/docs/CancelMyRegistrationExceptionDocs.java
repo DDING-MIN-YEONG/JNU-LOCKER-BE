cad282e7af5596ba5f79f3947cf527b7487fd5bd
Profile-exe <kang67572346@gmail.com>
2025-05-01 14:50:49 +0900

    style: spotless

--- src/main/java/com/jnulocker/registration/adapter/in/docs/CancelMyRegistrationExceptionDocs.java
+++ src/main/java/com/jnulocker/registration/adapter/in/docs/CancelMyRegistrationExceptionDocs.java
@@ -17,5 +17,6 @@ public class CancelMyRegistrationExceptionDocs implements SwaggerExceptionDoc {
     public static final BusinessException 이벤트가_존재하지_않을_때 = EventNotFoundException.EXCEPTION;
 
     @ExplainError("사물함 신청 내역이 존재하지 않을 때 발생하는 예외입니다")
-    public static final BusinessException 신청_내역이_존재하지_않을_때 = RegistrationNotFoundException.EXCEPTION;
+    public static final BusinessException 신청_내역이_존재하지_않을_때 =
+            RegistrationNotFoundException.EXCEPTION;
 }
