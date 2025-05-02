d3cd8628138fa9a134f761f2829ff9400379fda2
westzeroright <kt8267@naver.com>
2025-05-02 14:14:58 +0900

    refactor: 스웨거에서 불필요한 codeValid 보이는 현상 제거

--- src/main/java/com/jnulocker/auth/application/port/in/request/VerifyCodeRequest.java
+++ src/main/java/com/jnulocker/auth/application/port/in/request/VerifyCodeRequest.java
@@ -1,5 +1,6 @@
 package com.jnulocker.auth.application.port.in.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -8,6 +9,7 @@ public record VerifyCodeRequest(
         @NotBlank(message = "이메일은 필수 입력 값입니다") @Email(message = "올바른 형식의 이메일 주소여야 합니다")
                 String email,
         @NotBlank(message = "인증 코드는 필수 입력 값입니다") String code) {
+    @Schema(hidden = true)
     @AssertTrue(message = "인증 코드는 100000에서 999999 사이의 6자리 숫자여야 합니다")
     public boolean isCodeValid() {
         if (code == null) {
