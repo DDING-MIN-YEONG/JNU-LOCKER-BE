f6cd606c142a888c18d040aae5e239d2b06ba66f
westzeroright <kt8267@naver.com>
2025-05-05 18:19:44 +0900

    feat: 권한에 따른 응답 본문 제공

--- src/main/java/com/jnulocker/member/application/port/in/response/MemberInfoResponse.java
+++ src/main/java/com/jnulocker/member/application/port/in/response/MemberInfoResponse.java
@@ -1,6 +1,8 @@
 package com.jnulocker.member.application.port.in.response;
 
 import com.jnulocker.member.domain.Member;
+import com.jnulocker.member.domain.Role;
+import com.jnulocker.organization.domain.OrganizationType;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record MemberInfoResponse(
@@ -11,11 +13,27 @@ public record MemberInfoResponse(
         @Schema(description = "전화번호", example = "010-1234-1234") String phoneNumber,
         @Schema(description = "이메일", example = "test@example.com") String email) {
     public static MemberInfoResponse from(Member member) {
+        String studentNumber = null;
+        String affiliation = null;
+
+        Role role = member.getRole();
+        OrganizationType orgType = member.getDepartment().getOrganization().getType();
+
+        if (role == Role.USER) {
+            studentNumber = member.getStudentNumber();
+            affiliation = member.getDepartment().getName();
+        } else if (role == Role.MANAGER) { // 쉽게 테스트하려면 GUEST로 권한 바꾸기
+            if (orgType == OrganizationType.COUNCIL) {
+                studentNumber = member.getStudentNumber();
+            }
+            affiliation = member.getDepartment().getNickname();
+        }
+
         return new MemberInfoResponse(
                 member.getId(),
                 member.getName(),
-                member.getStudentNumber(),
-                member.getDepartment().getName(),
+                studentNumber,
+                affiliation,
                 member.getPhoneNumber(),
                 member.getEmail());
     }
