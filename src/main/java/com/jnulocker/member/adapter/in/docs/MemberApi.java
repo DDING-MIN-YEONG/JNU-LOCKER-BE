f2bdc7e28c274256f93b84fba6032b9489ae8f3f
westzeroright <kt8267@naver.com>
2025-05-05 15:50:42 +0900

    docs: 회원 정보 조회 API 문서 작성

---
+++ src/main/java/com/jnulocker/member/adapter/in/docs/MemberApi.java
@@ -0,0 +1,30 @@
+package com.jnulocker.member.adapter.in.docs;
+
+import com.jnulocker.member.application.port.in.response.MemberInfoResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "회원 API", description = "회원 관련 API")
+public interface MemberApi {
+
+    @Operation(summary = "회원 정보 조회", description = "회원 정보를 조회합니다.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "회원 정보 조회 성공",
+            content =
+                    @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array =
+                                    @ArraySchema(
+                                            schema =
+                                                    @Schema(
+                                                            implementation =
+                                                                    MemberInfoResponse.class))))
+    ResponseEntity<MemberInfoResponse> getMemberInfo();
+}
