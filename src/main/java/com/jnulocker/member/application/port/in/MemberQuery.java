f391a0411fbad2458ca0c267f5f13df3d1a7e13f
Profile-exe <kang67572346@gmail.com>
2025-05-08 00:33:16 +0900

    feat: 권한과 학과로 회원을 조회하는 Query 구현

--- src/main/java/com/jnulocker/member/application/port/in/MemberQuery.java
+++ src/main/java/com/jnulocker/member/application/port/in/MemberQuery.java
@@ -2,6 +2,10 @@ package com.jnulocker.member.application.port.in;
 
 import com.jnulocker.member.application.port.in.response.MemberInfoResponse;
 import com.jnulocker.member.domain.Member;
+import com.jnulocker.member.domain.Role;
+import com.jnulocker.organization.domain.Department;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface MemberQuery {
 
@@ -14,4 +18,6 @@ public interface MemberQuery {
     Member findByIdWithDepartmentOrThrow(Long memberId);
 
     MemberInfoResponse getMemberInfo();
+
+    Page<Member> getByRoleAndDepartment(Role role, Department department, Pageable pageable);
 }
