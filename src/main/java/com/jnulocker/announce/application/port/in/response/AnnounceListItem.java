ed5ee56599d1ea2d8b7764713fa4eaadc7fc2b2a
westzeroright <kt8267@naver.com>
2025-05-11 15:37:52 +0900

    feat: 공지사항 목록 조회 시 등록일자 필드 추가

--- src/main/java/com/jnulocker/announce/application/port/in/response/AnnounceListItem.java
+++ src/main/java/com/jnulocker/announce/application/port/in/response/AnnounceListItem.java
@@ -2,14 +2,20 @@ package com.jnulocker.announce.application.port.in.response;
 
 import com.jnulocker.announce.domain.Announce;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
 
 public record AnnounceListItem(
         @Schema(description = "공지사항 ID", example = "1") Long id,
         @Schema(description = "공지사항 제목", example = "백도 사물함 신청 안내") String title,
         @Schema(description = "공지사항 내용", example = "공지사항 내용입니다.") String content,
-        @Schema(description = "공지사항 작성자", example = "도서관자치위원회 백문이불여일견") String writer) {
+        @Schema(description = "공지사항 작성자", example = "도서관자치위원회 백문이불여일견") String writer,
+        @Schema(description = "등록 일자", example = "2025-08-01T15:00:00") LocalDateTime createdAt) {
     public static AnnounceListItem from(Announce announce) {
         return new AnnounceListItem(
-                announce.getId(), announce.getTitle(), announce.getContent(), announce.getWriter());
+                announce.getId(),
+                announce.getTitle(),
+                announce.getContent(),
+                announce.getWriter(),
+                announce.getCreatedAt());
     }
 }
