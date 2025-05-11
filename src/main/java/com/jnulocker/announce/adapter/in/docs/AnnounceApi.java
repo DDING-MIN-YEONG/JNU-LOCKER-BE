b677768d69284fa26bd21269f94721a2da9fb0a4
westzeroright <kt8267@naver.com>
2025-05-11 16:02:52 +0900

    docs: 공지사항 목록 조회(user) API 문서 작성

--- src/main/java/com/jnulocker/announce/adapter/in/docs/AnnounceApi.java
+++ src/main/java/com/jnulocker/announce/adapter/in/docs/AnnounceApi.java
@@ -3,13 +3,16 @@ package com.jnulocker.announce.adapter.in.docs;
 import com.jnulocker.announce.application.port.in.request.CreateAnnounceRequest;
 import com.jnulocker.announce.application.port.in.response.AnnounceCustomPage;
 import com.jnulocker.announce.application.port.in.response.AnnouncePageable;
+import com.jnulocker.announce.application.port.in.response.MyAnnounceResponse;
 import com.jnulocker.common.swagger.ApiExceptionExamples;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -33,4 +36,20 @@ public interface AnnounceApi {
                             schema = @Schema(implementation = AnnounceCustomPage.class)))
     ResponseEntity<AnnounceCustomPage> getAnnounces(
             @Valid @ParameterObject AnnouncePageable announcePageable);
+
+    @ApiExceptionExamples(GetMyAnnouncesExceptionDocs.class)
+    @Operation(summary = "자신의 소속학과 대상 공지사항 목록 조회", description = "자신의 소속학과가 참여한 공지사항 목록을 조회합니다.")
+    @ApiResponse(
+            responseCode = "200",
+            description = "자신의 소속학과 대상 공지사항 목록 조회 성공",
+            content =
+                    @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array =
+                                    @ArraySchema(
+                                            schema =
+                                                    @Schema(
+                                                            implementation =
+                                                                    MyAnnounceResponse.class))))
+    ResponseEntity<List<MyAnnounceResponse>> getMyAnnounces();
 }
