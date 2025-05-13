cc37d871ac65dd4d9194f36f64cdc2e9c60c40ce
Profile-exe <kang67572346@gmail.com>
2025-05-13 22:51:27 +0900

    feat: 내 이벤트 조회 쿼리 publish true 조건 추가 및 정렬 적용

--- src/main/java/com/jnulocker/events/adapter/out/EventRepositoryCustomImpl.java
+++ src/main/java/com/jnulocker/events/adapter/out/EventRepositoryCustomImpl.java
@@ -4,9 +4,11 @@ import static com.jnulocker.events.domain.QEvent.event;
 import static com.jnulocker.events.domain.QEventParticipation.eventParticipation;
 import static com.jnulocker.events.domain.QLocker.locker;
 
+import com.jnulocker.common.persistence.querydsl.QueryDslOrderUtils;
 import com.jnulocker.events.application.port.in.response.MyEventCustomPage;
 import com.jnulocker.events.application.port.in.response.MyEventListItem;
 import com.jnulocker.events.application.port.in.response.QMyEventListItem;
+import com.jnulocker.events.infrastructure.querydsl.EventPathMap;
 import com.jnulocker.organization.domain.Department;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -31,7 +33,7 @@ public class EventRepositoryCustomImpl implements EventRepositoryCustom {
                         .select(event.count())
                         .from(event)
                         .join(event.eventParticipations, eventParticipation)
-                        .where(eventParticipation.department.eq(department))
+                        .where(eventParticipation.department.eq(department), event.publish.isTrue())
                         .fetchOne();
 
         List<MyEventListItem> content =
@@ -51,7 +53,10 @@ public class EventRepositoryCustomImpl implements EventRepositoryCustom {
                                                         locker.floor.event.id.eq(event.id))))
                         .from(event)
                         .join(event.eventParticipations, eventParticipation)
-                        .where(eventParticipation.department.eq(department))
+                        .where(eventParticipation.department.eq(department), event.publish.isTrue())
+                        .orderBy(
+                                QueryDslOrderUtils.getOrderSpecifiers(
+                                        pageable, EventPathMap.getInstance()))
                         .offset(offset)
                         .limit(pageSize)
                         .fetch();
