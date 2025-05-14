b71457d5b5e6b2dac58f7a26fae0b9420afee296
Profile-exe <kang67572346@gmail.com>
2025-05-15 01:05:51 +0900

    test: 이벤트 상세 조회 테스트케이스 작성

--- src/test/java/com/jnulocker/events/adapter/in/EventControllerIntegrationTest.java
+++ src/test/java/com/jnulocker/events/adapter/in/EventControllerIntegrationTest.java
@@ -10,8 +10,10 @@ import com.jnulocker.events.application.port.in.request.CreateEventRequest;
 import com.jnulocker.events.application.port.in.request.PublishEventRequest;
 import com.jnulocker.events.application.port.in.response.EventCustomPage;
 import com.jnulocker.events.application.port.in.response.EventPageable;
+import com.jnulocker.events.application.port.in.response.EventResponse;
 import com.jnulocker.events.application.port.in.response.FloorWithLockersResponse;
 import com.jnulocker.events.application.port.in.response.MyEventCustomPage;
+import com.jnulocker.events.application.port.in.response.MyEventListItem;
 import com.jnulocker.events.domain.Event;
 import com.jnulocker.events.domain.EventStatus;
 import com.jnulocker.events.exception.EventErrorCode;
@@ -370,6 +372,49 @@ public class EventControllerIntegrationTest {
         assertThat(myEvents.content()).isEmpty();
         assertThat(myEvents.totalElements()).isZero();
         assertThat(myEvents.last()).isTrue();
+    }
+
+    @Test
+    void 이벤트_상세조회가_가능하다() {
+        // given
+        Department department = organizationTestUtil.createCouncilDepartment();
+        Member member = memberTestUtil.createMemberFromRoleWithDepartment(Role.USER, department);
+
+        // 이벤트 생성: 짝수번 사물함은 사용 가능, 홀수번 사물함은 사용 불가능
+        // 15개 중 가능한 사물함 수는 2, 4, 6, 8, 10, 12, 14 (총 7개)
+        eventTestUtil.createEventWithParticipationDepartment(
+                List.of(5, 10), department, EventStatus.OPEN, true);
+
+        // when
+        accessToken = authTestUtil.generateAccessTokenWithMember(member);
+        EventPageable eventPageable = new EventPageable(0, 10, DIRECTION, SORT);
+        MyEventCustomPage myEvents =
+                getMyEventsWithPageable(accessToken, eventPageable)
+                        .statusCode(HttpStatus.OK.value())
+                        .extract()
+                        .as(MyEventCustomPage.class);
+
+        MyEventListItem myEvent = myEvents.content().getFirst();
+        EventResponse eventResponse =
+                getEventDetail(myEvent.id(), accessToken)
+                        .statusCode(HttpStatus.OK.value())
+                        .extract()
+                        .as(EventResponse.class);
+
+        // then
+        // 자신의 소속 학과가 참여하는 이벤트가 조회되었는지 확인
+        assertThat(eventResponse.status()).isEqualTo(EventStatus.OPEN);
+        assertThat(eventResponse.departmentIds()).containsExactly(department.getId());
+    }
+
+    private static ValidatableResponse getEventDetail(UUID eventId, String accessToken) {
+        return given().contentType(MediaType.APPLICATION_JSON_VALUE)
+                .cookie(new Cookie.Builder(ACCESS_TOKEN, accessToken).build())
+                .when()
+                .get(EVENT_URL + "/{event-id}", eventId)
+                .then()
+                .log()
+                .all();
     }
 
     // 파라미터 제공 메서드
