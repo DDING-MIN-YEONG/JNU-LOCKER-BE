9af8ad152c9bf7d11c207dacff19c90e2dc14a97
Profile-exe <kang67572346@gmail.com>
2025-05-15 01:26:04 +0900

    test: MANAGER 가입 승인 거절 테스트케이스 작성

--- src/test/java/com/jnulocker/auth/adapter/in/AuthControllerIntegrationTest.java
+++ src/test/java/com/jnulocker/auth/adapter/in/AuthControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThatList;
 import com.jnulocker.auth.adapter.out.TokenRepository;
 import com.jnulocker.auth.application.port.in.request.LoginRequest;
 import com.jnulocker.auth.application.port.in.request.ManagerApproveRequest;
+import com.jnulocker.auth.application.port.in.request.ManagerRejectRequest;
 import com.jnulocker.auth.application.port.in.request.ManagerSignupRequest;
 import com.jnulocker.auth.application.port.in.request.SendEmailRequest;
 import com.jnulocker.auth.application.port.in.request.UserSignupRequest;
@@ -687,6 +688,39 @@ class AuthControllerIntegrationTest {
         // then
         assertThat(errorResponse.message())
                 .isEqualTo(AuthErrorCode.ONLY_SAME_DEPARTMENT_CAN_APPROVE.getMessage());
+    }
+
+    @Test
+    void MANAGER는_가입_승인_요청을_거절할_수_있다() {
+        // given
+        Department department = organizationTestUtil.createCouncilDepartment();
+        ManagerSignupRequest signupRequest =
+                managerSignupRequestBuilder().withDepartmentId(department.getId()).build();
+        signupManager(signupRequest).statusCode(HttpStatus.CREATED.value());
+
+        // 가입 요청한 학생회 회원의 정보 조회
+        Member approveeMember = memberTestUtil.findMemberByEmail(signupRequest.email());
+        String accessToken =
+                authTestUtil.generateAccessTokenWithDepartment(Role.MANAGER, department);
+
+        ManagerRejectRequest rejectRequest = new ManagerRejectRequest(approveeMember.getId());
+
+        // when, then
+        rejectManagerSignup(accessToken, rejectRequest).statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    private ValidatableResponse rejectManagerSignup(
+            String accessToken, ManagerRejectRequest request) {
+        return given().contentType(MediaType.APPLICATION_JSON_VALUE)
+                .cookie(new Cookie.Builder(ACCESS_TOKEN, accessToken).build())
+                .body(request)
+                .log()
+                .all()
+                .when()
+                .delete(AUTH_URL + "/managers/approve")
+                .then()
+                .log()
+                .all();
     }
 
     private ValidatableResponse approveManagerSignup(
