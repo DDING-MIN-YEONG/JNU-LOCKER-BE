28055d61301df92ad7338e84612441bdf7815a0f
Profile-exe <kang67572346@gmail.com>
2025-05-15 01:28:22 +0900

    fix: 올바른 Param 어노테이션 사용

--- src/main/java/com/jnulocker/events/adapter/out/EventRepository.java
+++ src/main/java/com/jnulocker/events/adapter/out/EventRepository.java
@@ -2,13 +2,13 @@ package com.jnulocker.events.adapter.out;
 
 import com.jnulocker.events.domain.Event;
 import com.jnulocker.organization.domain.Department;
-import io.lettuce.core.dynamic.annotation.Param;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<Event, UUID>, EventRepositoryCustom {
 
