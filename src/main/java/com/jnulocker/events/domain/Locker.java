b5d76ea46da28000844f57a2e09ebc0fc22f6788
Profile-exe <kang67572346@gmail.com>
2025-05-17 00:54:17 +0900

    feat: Locker 삭제 전 명시적으로 Registration 삭제

--- src/main/java/com/jnulocker/events/domain/Locker.java
+++ src/main/java/com/jnulocker/events/domain/Locker.java
@@ -2,8 +2,6 @@ package com.jnulocker.events.domain;
 
 import com.jnulocker.common.persistence.BaseEntity;
 import com.jnulocker.events.exception.LockerUnavailableException;
-import com.jnulocker.registration.domain.Registration;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -12,7 +10,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -38,9 +35,6 @@ public class Locker extends BaseEntity {
     private String code;
 
     private Boolean available;
-
-    @OneToOne(mappedBy = "locker", orphanRemoval = true, cascade = CascadeType.ALL)
-    private Registration registration;
 
     public static Locker create(Floor floor, String code, Boolean available) {
         return Locker.builder().floor(floor).code(code).available(available).build();
