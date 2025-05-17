025eac8d5963a9b2dd34f518e403767cea3728ca
Profile-exe <kang67572346@gmail.com>
2025-05-17 16:50:20 +0900

    docs: 수정 API 예외 문서 수정

--- src/main/java/com/jnulocker/events/adapter/in/docs/UpdateEventExceptionDocs.java
+++ src/main/java/com/jnulocker/events/adapter/in/docs/UpdateEventExceptionDocs.java
@@ -4,9 +4,9 @@ import com.jnulocker.common.exception.BusinessException;
 import com.jnulocker.common.swagger.ExceptionDoc;
 import com.jnulocker.common.swagger.ExplainError;
 import com.jnulocker.common.swagger.SwaggerExceptionDoc;
-import com.jnulocker.events.exception.CloseEventCannotBeUpdatedException;
 import com.jnulocker.events.exception.EventNotFoundException;
 import com.jnulocker.events.exception.OnlyManagerCanUpdateEventException;
+import com.jnulocker.events.exception.OnlyReadyEventCanBeUpdatedException;
 import com.jnulocker.member.exception.MemberNotFoundException;
 import com.jnulocker.organization.exception.DepartmentNotFoundException;
 import lombok.AccessLevel;
@@ -26,9 +26,9 @@ public class UpdateEventExceptionDocs implements SwaggerExceptionDoc {
     public static final BusinessException 이벤트_주최_학과가_아닐_때 =
             OnlyManagerCanUpdateEventException.EXCEPTION;
 
-    @ExplainError("이벤트가 종료된 상태에서 수정 요청이 들어올 때 발생하는 예외입니다")
-    public static final BusinessException 종료된_이벤트는_수정할_수_없을_때 =
-            CloseEventCannotBeUpdatedException.EXCEPTION;
+    @ExplainError("이벤트가 OPEN, CLOSE 상태에서 수정 요청이 들어올 때 발생하는 예외입니다")
+    public static final BusinessException 이벤트가_OPEN_CLOSE_상태에서_수정_요청이_들어올_때 =
+            OnlyReadyEventCanBeUpdatedException.EXCEPTION;
 
     @ExplainError("이벤트 주관 학과가 존재하지 않거나 이벤트 참여 학과가 존재하지 않을 때 발생하는 예외입니다")
     public static final BusinessException 이벤트_주관_학과가_존재하지_않거나_이벤트_참여_학과가_존재하지_않을_때 =
