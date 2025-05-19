19dbec9335fa92c93dd3b32ffd2045b5cba24543
westzeroright <kt8267@naver.com>
2025-05-19 14:26:13 +0900

    refactor: 로그를 남기지 않으므로 불필요한 어노테이션 제거

--- src/main/java/com/jnulocker/events/event/LockerEventCreatedEventHandler.java
+++ src/main/java/com/jnulocker/events/event/LockerEventCreatedEventHandler.java
@@ -3,12 +3,10 @@ package com.jnulocker.events.event;
 import com.jnulocker.events.quartz.QuartzSchedulerUtil;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class LockerEventCreatedEventHandler {
