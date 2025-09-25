434437f643d2925783a74a65d660acd340a2243a
Profile-exe <kang67572346@gmail.com>
2025-09-26 02:48:39 +0900

    feat: 사물함 엔티티 Comparable 인터페이스 구현해 정렬 기준 적용
    - 1순위: 접두사 순
    - 2순위: 사물함 번호 순

--- src/main/java/com/jnulocker/events/domain/Locker.java
+++ src/main/java/com/jnulocker/events/domain/Locker.java
@@ -22,7 +22,7 @@ import lombok.NoArgsConstructor;
 @Builder(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Locker extends BaseEntity {
+public class Locker extends BaseEntity implements Comparable<Locker> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -57,6 +57,40 @@ public class Locker extends BaseEntity {
     private void checkAvailability() {
         if (Boolean.FALSE.equals(available)) {
             throw LockerUnavailableException.EXCEPTION;
+        }
+    }
+
+    @Override
+    public int compareTo(Locker other) {
+        String[] thisCodeParts = parseCode(this.code);
+        String[] otherCodeParts = parseCode(other.code);
+
+        String thisPrefix = thisCodeParts[0];
+        String otherPrefix = otherCodeParts[0];
+        int thisNumber = Integer.parseInt(thisCodeParts[1]);
+        int otherNumber = Integer.parseInt(otherCodeParts[1]);
+
+        // 1. 접두사 비교 (null인 경우 빈 문자열로 처리)
+        String safeThisPrefix = thisPrefix != null ? thisPrefix : "";
+        String safeOtherPrefix = otherPrefix != null ? otherPrefix : "";
+        int prefixComparison = safeThisPrefix.compareTo(safeOtherPrefix);
+
+        if (prefixComparison != 0) {
+            return prefixComparison;
+        }
+
+        // 2. 번호 비교 (숫자로 비교)
+        return Integer.compare(thisNumber, otherNumber);
+    }
+
+    private String[] parseCode(String code) {
+        if (code.contains("-")) {
+            // 접두사가 있는 경우: "A-001" -> ["A", "001"]
+            String[] parts = code.split("-", 2);
+            return new String[]{parts[0], parts[1]};
+        } else {
+            // 접두사가 없는 경우: "001" -> [null, "001"]
+            return new String[]{null, code};
         }
     }
 }
