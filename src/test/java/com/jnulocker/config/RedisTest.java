090c9c187810d8d38a98c165a0a32e40e377891d
Profile-exe <kang67572346@gmail.com>
2025-11-28 22:15:30 +0900

    test: ManagerEmailSender 모킹

--- src/test/java/com/jnulocker/config/RedisTest.java
+++ src/test/java/com/jnulocker/config/RedisTest.java
@@ -1,11 +1,15 @@
 package com.jnulocker.config;
 
+import com.jnulocker.auth.application.port.out.ManagerEmailSender;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 @Import({RedisTestConfig.class, TestVectorStoreConfig.class})
 public abstract class RedisTest extends RedisTestContainer {
+
+    @MockitoBean protected ManagerEmailSender managerEmailSender;
 }
