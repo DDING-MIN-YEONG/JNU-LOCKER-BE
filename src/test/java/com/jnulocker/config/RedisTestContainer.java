b751093bd2c69706a6c3a8cffd6bec5ce4af25f2
Profile-exe <kang67572346@gmail.com>
2025-11-28 22:16:14 +0900

    test: Redis 테스트 컨테이너의 reuse 옵션을 제거
    - 컨테이너가 계속 실행되며 남아있지 않도록 설정

--- src/test/java/com/jnulocker/config/RedisTestContainer.java
+++ src/test/java/com/jnulocker/config/RedisTestContainer.java
@@ -3,10 +3,8 @@ package com.jnulocker.config;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-@Testcontainers
 public abstract class RedisTestContainer {
 
     private static final int REDIS_PORT = 6379;
@@ -16,8 +14,7 @@ public abstract class RedisTestContainer {
     static {
         REDIS_CONTAINER =
                 new GenericContainer<>(DockerImageName.parse(REDIS_IMAGE))
-                        .withExposedPorts(REDIS_PORT)
-                        .withReuse(true);
+                        .withExposedPorts(REDIS_PORT);
         REDIS_CONTAINER.start();
     }
 
